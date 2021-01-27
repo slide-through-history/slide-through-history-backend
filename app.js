b2a8dd1f4c927559506a2f584887e1b62293bcbe
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-01-27 11:53:06 -0500

    Add session middleware

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const debug = require("debug")(
   `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
 const app = express();
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+
+
 // CORS setup
 app.use(
   cors({
@@ -28,6 +33,18 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 require("./configs/db.config.js");
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60 * 1000 }, // 60 seconds
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    resave: true,
+    saveUninitialized: false,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 
 // Route setup
 app.use("/", require("./routes/index.js"));
